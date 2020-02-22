44ea79530b8491db3b6da95edef76b24ea38514b
Clarence Rouvel <clarence.rouvel@gmail.com>
2020-02-22 22:16:49 +0100

    views + app.js + layout.html

--- music_managerServer/src/app.js
+++ music_managerServer/src/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var mongoose = require ('mongoose');
 var app = express();
+var nunjucks = require('nunjucks');
 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
+});
 app.get('/',(req, res) =>{
 	res.send('salut');
 })
