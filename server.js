030d2131ffdb4f847681364a9abf39ac18902254
crouvel <55972660+crouvel@users.noreply.github.com>
2020-02-28 18:51:42 +0100

    Modif server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const Express = require('express');
 
 let app = Express();
 
+const PORT = process.env.PORT || 3000;
+
 const userRoute = require('./routes/User.js')
 
 app.use('/user', userRoute);
 
-app.listen(3000);
+app.listen(PORT);
