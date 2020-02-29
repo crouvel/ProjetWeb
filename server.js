95951373b2e29bcd64ae642f1058a9d0fee7966a
Clarence Rouvel <clarence.rouvel@gmail.com>
2020-02-29 18:11:39 +0100

    modifs server.js

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const Express = require('express');
-
-let app = Express();
-
+const { Sequelize } = require('sequelize'); 
 const PORT = process.env.PORT || 3000;
+const app = Express();
 
 const userRoute = require('./routes/User.js')
+const sequelize = new Sequelize('prj', 'root', 'YES', {
+  host: 'localhost',
+  dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+
+
+
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.use('/user', userRoute);
 
