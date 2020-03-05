c94bc3f5095afb4bb04e2749f6b9f6bf8f3df42b
crouvel <55972660+crouvel@users.noreply.github.com>
2020-03-05 22:09:47 +0100

    app

--- projet/app.js
+++ projet/app.js
@@ -28,7 +28,7 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/songUpload', songUploadRouter);
-app.use('/home', homeRouter);
+app.use('/Home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
