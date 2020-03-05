4b0f77ab99f8678e52d5f40e1fb137d5dfcc7e7a
crouvel <55972660+crouvel@users.noreply.github.com>
2020-03-05 21:40:36 +0100

    adding route

--- projet/app.js
+++ projet/app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
 var songUploadRouter = require('./routes/songUpload');
+var homeRouter = require('./routes/home');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/songUpload', songUploadRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
