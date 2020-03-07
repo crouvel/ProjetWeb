0b0f6be34e284ad94e16bca55262503612715ca1
crouvel <55972660+crouvel@users.noreply.github.com>
2020-03-07 13:59:11 +0100

    album

--- projet/routes/addAlbum.js
+++ projet/routes/addAlbum.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('updateAlbum', { title: 'Music Manager' });
+  res.render('addAlbum', { title: 'Music Manager' });
 });
 
 module.exports = router;
